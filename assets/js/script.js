33704b4725dbb20f3cd024e2b845bc6701d849ca
JEZA <jesus.zuniga077@gmail.com>
2024-04-29 22:13:53 -0600

    mejoras

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,81 @@
 'use strict';
 
+//** TIMER */
+window.onload = function() {
+  let time = 60 * 20.8; // Tiempo en segundos (1 hora)
+  const display = document.querySelector('.timer');
 
+  const timer = setInterval(function () {
+    const hours = parseInt(time / 3600, 10);
+    const minutes = parseInt((time % 3600) / 60, 10);
+    const seconds = parseInt(time % 60, 10);
 
-/**
- * add event on element
- */
+    document.getElementById('hours').textContent = hours < 10 ? "0" + hours : hours;
+    document.getElementById('minutes').textContent = minutes < 10 ? "0" + minutes : minutes;
+    document.getElementById('seconds').textContent = seconds < 10 ? "0" + seconds : seconds;
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+    if (--time < 0) {
+      clearInterval(timer);
     }
-  } else {
-    elem.addEventListener(type, callback);
-  }
+  }, 1000);
 }
 
+// /**
+//  * add event on element
+//  */
 
-
-/**
- * navbar toggle
- */
-
-const navToggler = document.querySelector("[data-nav-toggler]");
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-}
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNavbar);
+// const addEventOnElem = function (elem, type, callback) {
+//   if (elem.length > 1) {
+//     for (let i = 0; i < elem.length; i++) {
+//       elem[i].addEventListener(type, callback);
+//     }
+//   } else {
+//     elem.addEventListener(type, callback);
+//   }
+// }
 
 
 
-/**
- * active header when window scroll down to 100px
- */
+// /**
+//  * navbar toggle
+//  */
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+// const navToggler = document.querySelector("[data-nav-toggler]");
+// const navbar = document.querySelector("[data-navbar]");
+// const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-const activeElemOnScroll = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
+// const toggleNavbar = function () {
+//   navbar.classList.toggle("active");
+//   navToggler.classList.toggle("active");
+// }
 
-addEventOnElem(window, "scroll", activeElemOnScroll);
+// addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+// const closeNavbar = function () {
+//   navbar.classList.remove("active");
+//   navToggler.classList.remove("active");
+// }
+
+// addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+// /**
+//  * active header when window scroll down to 100px
+//  */
+
+// const header = document.querySelector("[data-header]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+// const activeElemOnScroll = function () {
+//   if (window.scrollY > 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// }
+
+// addEventOnElem(window, "scroll", activeElemOnScroll);
